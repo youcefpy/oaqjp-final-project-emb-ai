50c5b714a6542d1e0b329d67d0adc9644ebc9047
youcefpy <youcefboutiche@hotmail.fr>
2026-08-25 05:48:52 -0400

    Format the output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,7 +1,7 @@
 import requests 
+import json
 
-
-def emotion_detection(text_to_analyze):
+def emotion_detector(text_to_analyze):
     
     text_to_analyze = text_to_analyze
     
@@ -18,4 +18,25 @@ def emotion_detection(text_to_analyze):
 
     response = requests.post(url, json = myobj, headers=headers)
 
-    return response.text
+    # Convert the response text into a dictionary using the json library functions. 
+    response_obj = json.loads(response.text)
+    
+    # Extract the required set of emotions, including anger, disgust, fear, joy and sadness, along with their scores.
+    emotions_info = response_obj.get('emotionPredictions')[0]
+    
+    emotions = emotions_info.get("emotion")
+    
+    # find the dominant emotion, which is the emotion with the highest score.
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    # dict with all emotions including the dominant emotion
+    dict_ = {
+        'anger' : emotions['anger'],
+        'disgust' : emotions['disgust'],
+        'fear' : emotions['fear'],
+        'joy' : emotions['joy'],
+        'sadness' : emotions['sadness'],
+        "dominant_emotion" : dominant_emotion
+    }
+
+    return dict_
